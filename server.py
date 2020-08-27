8a687dd858e62c02457d964d9f0e375d1be31049
CodyCline <clinecody98@gmail.com>
2020-08-27 14:14:15 -0700

    feat(backend) Add custom gaussian function, trim and return location route

--- server.py
+++ server.py
@@ -13,5 +13,43 @@ def quantum_seed():
     return int("".join(val))
 
 
+def gauss(mu, sigma, seed):
+        #Pass seed from API call
+        random.seed(seed)
+        gauss_next = None
+        z = gauss_next
+        if z is None:
+            x2pi = random.random() * math.tau 
+            g2rad = math.sqrt(-2.0 * math.log(1.0 - random.random()))
+            z = math.cos(x2pi) * g2rad
+            gauss_next = math.sin(x2pi) * g2rad
+
+        return mu + z * sigma
+
+def trim(lat, lon):
+    #Round numbers to nearest hundreth thousand then multiply
+    return {
+        "latitude": int(round(lat, 6) * 1000000),
+        "longitude": int(round(lon, 6) * 1000000)
+    }
+
+
+@app.route("/location")
+def get_location():
+    seed = quantum_seed()
+    print(request.args.get("latitude"), request.args.get("longitude"))
+    mean = float(request.args.get("mean"))
+    v = 1000000.0 #Constant for dividing to nearest hundred thousandths place
+    coords = trim(float(request.args.get("latitude")), float(request.args.get("longitude")))
+    latitude = int(gauss(coords["latitude"], mean, seed))
+    longitude = int(gauss(coords["longitude"], mean, seed))   
+    
+    return ({
+        "latitude": latitude / v,
+        "longitude": longitude / v,
+    })
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
